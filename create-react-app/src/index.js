01ab5651b5db862ffd25354654cfea14863ecbc9
x03177 <x03177@gmail.com>
2019-12-27 12:44:49 +0800

    MT1st-12 React LifeCycle

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoList from './TodoList';
+import LifeCycle from './LifeCycle';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<LifeCycle />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
